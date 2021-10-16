85e5552d557506f76e93796c73a5d0fe4cc84c20
kenil333 <keniljasani19@gmail.com>
2021-10-16 13:42:53 +0530

    compters_react_o2

--- src/component/jsfiles/BlogDes.js
+++ src/component/jsfiles/BlogDes.js
@@ -15,6 +15,12 @@ function BlogDes() {
                 <p className="blog__body__des">{props.description1}</p>
                 <center><img src={props.image} alt='Something is wrong' className="blog__body__image" /></center>
                 <p className="blog__body__des">{props.description2}</p>
+                <p className="blog__body__des">{props.description3 === 'No' ? '' :props.description3}</p>
+                {props.exmple ? <div>
+                    <p className="blog__exm">Example :</p>
+                    <p className="blog__exm__tit">{props.exmtit}</p>
+                    <p className="blog__body__des">{props.exmdes}</p>
+                </div> : <div></div>}
             </div>
             <Getaccount />
             <Footer />
